b6056361a3184b312c70f00f2fc9b6f22d74b933
nitish kashyap <kashyapnitishkn@gmail.com>
2025-05-10 00:13:52 +0530

    bug id 2

--- src/infopack/HelloWorld.java
+++ src/infopack/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("first time here");
-        System.out.println("secomnd time here");
+        System.out.println("secomnd time ");
         
     }
 }
