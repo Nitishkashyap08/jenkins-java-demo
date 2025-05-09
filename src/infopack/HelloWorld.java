674695ae5dd53de33846f8d4d97f1b7334cac013
nitish kashyap <kashyapnitishkn@gmail.com>
2025-05-09 23:56:51 +0530

    Intial commmit

---
+++ src/infopack/HelloWorld.java
@@ -0,0 +1,6 @@
+package infopack;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kashy
+ *
+ */
+module Jenkins {
+}
