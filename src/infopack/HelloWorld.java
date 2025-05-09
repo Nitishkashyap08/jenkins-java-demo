f9510a36be0cb58603f6a968f779481abfa2b628
nitish kashyap <kashyapnitishkn@gmail.com>
2025-05-10 00:06:38 +0530

    bug fix-1

--- src/infopack/HelloWorld.java
+++ src/infopack/HelloWorld.java
@@ -2,5 +2,7 @@ package infopack;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
+        System.out.println("first time here");
+        
     }
 }
