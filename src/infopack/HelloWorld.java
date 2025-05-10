fc87d72f14854e625bf76ca75bbb494c0cb53130
nitish kashyap <kashyapnitishkn@gmail.com>
2025-05-10 15:44:09 +0530

    commit

--- src/infopack/HelloWorld.java
+++ src/infopack/HelloWorld.java
@@ -1,9 +1,7 @@
 package infopack;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
-        System.out.println("first time here");
-        System.out.println("secomnd time ");
+        System.out.println("Hello from Jenkins! : version-1.1");
         
     }
 }
