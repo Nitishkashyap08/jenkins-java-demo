60e581aed922b37d2b4395ee6cbc0c24b0159362
nitish kashyap <kashyapnitishkn@gmail.com>
2025-05-10 16:28:20 +0530

    commp

--- src/infopack/HelloWorld.java
+++ src/infopack/HelloWorld.java
@@ -2,8 +2,8 @@ package infopack;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins! : version-1.2");
-        System.out.println("Hello from Jenkins! : version-2.2")
-        c = a_h';
+        System.out.println("Hello from Jenkins! : version-2.2");
+     
         
         
     }
